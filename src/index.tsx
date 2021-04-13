c8756b4dbce2966e059ceddd1c66d52d08a3d6ad
Trivium-bit <trivium.exe@gmail.com>
2021-04-13 22:36:48 +0300

    todolists_reducer_refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import './index.css';
 import AppWithRedux from './AppWithRedux';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
+import {store} from "./state/store";
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <AppWithRedux />
     </Provider>,
     document.getElementById('root'));
