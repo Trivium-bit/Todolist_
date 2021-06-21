9357f30aebcac41c224f3f062943dd3ed1e81216
Vitali Baliankou <trivium.exe@gmail.com>
2021-06-21 09:26:17 +0300

    useCallback

--- src/state/store.ts
+++ src/state/store.ts
@@ -8,12 +8,10 @@ const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 })
-
 // непосредственно создаём store
 export const store = createStore(rootReducer);
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
-
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
