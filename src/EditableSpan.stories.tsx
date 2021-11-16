71b5e00820245db22c99c99e06a9353743c9700b
Vitali Baliankou <trivium.exe@gmail.com>
2021-11-16 20:17:36 +0300

    storybook

---
+++ src/EditableSpan.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { EditableSpan, EditableSpanPropsType } from './EditableSpan';
+import {action} from "@storybook/addon-actions"
+
+export default {
+  title: 'Todolist/EditableSpan',
+  component: EditableSpan,
+  argTypes: {
+    onChange: {
+      description: 'Value EditableSpan changed'
+      },
+    value: {
+      defaultValue: 'HTML',
+      description: 'Start value EditableSpan'
+      }
+  },
+} as ComponentMeta<typeof EditableSpan>;
+
+const Template: ComponentStory<typeof EditableSpan> = (args: EditableSpanPropsType) => <EditableSpan {...args} />;
+
+export const EditableSpanExample = Template.bind({});
+EditableSpanExample.args = {
+onChange: action('Value EditableSpan changed')
+}
