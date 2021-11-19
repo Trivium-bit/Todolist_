5919fc285fdd7ed8fcfc7fa51617e96c2e116ed5
Trivium-bit <trivium.exe@gmail.com>
2021-11-19 11:59:26 +0300

    4

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -29,7 +29,7 @@ export const CreateTodolist =
         useEffect(() => {
             axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {
                 title:
-                    "newolist"
+                    "newTodolist"
             }, settings).then((res) => {
                 setState(res.data);
             })
@@ -40,9 +40,15 @@ export const DeleteTodolist =
     () => {
         const [state, setState] = useState<any>(null)
         useEffect(() => {
+            const todolistId = 'd5e75864-86be-48e1-a2a2-52414e99b879';
+            axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,
+                settings).then((res) => {
+                    setState(res.data);
+                })
         }, [])
         return <div> {JSON.stringify(state)}</div>
     }
+
 export const UpdateTodolistTitle =
     () => {
         const [state, setState] = useState<any>(null)
