b590b232b89290947a0a80727be89cb0a3aa5316
Trivium-bit <trivium.exe@gmail.com>
2022-02-04 10:09:49 +0300

    Snackbar

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Snackbar from '@material-ui/core/Snackbar'
 import MuiAlert, { AlertProps } from '@material-ui/lab/Alert'
+import { useSelector } from 'react-redux'
+import { AppRootStateType } from './../../app/store'
 
 function Alert(props: AlertProps) {
     return <MuiAlert elevation={6} variant="filled" {...props} />
 }
+
+const error = useSelector<AppRootStateType>(state => state.app.error)
 
 export function ErrorSnackbar() {
     const [open, setOpen] = React.useState(true)
@@ -16,7 +20,7 @@ export function ErrorSnackbar() {
     }
 
     return (
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Snackbar open={error !== null } autoHideDuration={6000} onClose={handleClose}>
             <Alert onClose={handleClose} severity="error">
                 This is a success message!
             </Alert>
