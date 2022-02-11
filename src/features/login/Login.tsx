a388999a67beedd9a88ba6ce9e09ace895f932c9
Trivium-bit <trivium.exe@gmail.com>
2022-02-11 12:34:39 +0300

    add formik

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -7,10 +7,40 @@ import FormGroup from '@mui/material/FormGroup';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useFormik } from 'formik';
+
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+    }
 
 export const Login = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            
+            if (!values.email) {
+            errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+            }
+            return errors;
+            },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        },
+    });
+
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
+            <form onSubmit={formik.handleSubmit}>
             <FormControl>
                 <FormLabel>
                     <p>To log in get registered
@@ -23,16 +53,16 @@ export const Login = () => {
                     <p>Password: free</p>
                 </FormLabel>
                 <FormGroup>
-                    <TextField label="Email" margin="normal" />
-                    <TextField type="password" label="Password"
-                        margin="normal"
-                    />
-                    <FormControlLabel label={'Remember me'} control={<Checkbox />} />
+                    <TextField label="Email" margin="normal" name="email" onChange={formik.handleChange} value={formik.values.email} />
+                    {formik.errors.email ? <div style={{color: 'red'}}>{formik.errors.email}</div> : null}
+                    <TextField type="password" label="Password" name="password" onChange={formik.handleChange} value={formik.values.password} margin="normal"/>
+                    <FormControlLabel label={'Remember me'} control={<Checkbox name="rememberMe" checked={formik.values.rememberMe} onChange={formik.handleChange} />} />
                     <Button type={'submit'} variant={'contained'} color={'primary'}>
                         Login
                     </Button>
                 </FormGroup>
             </FormControl>
+            </form>
         </Grid>
     </Grid>
 }
