c7464486a35e861723f6387ec5f8f772081a76da
Trivium-bit <trivium.exe@gmail.com>
2022-02-11 09:04:38 +0300

    Routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,30 +8,40 @@ import { AppRootStateType } from './store';
 import { useDispatch, useSelector } from 'react-redux'
 import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
 import { RequestStatusType } from './app-reducer'
+import { Login } from '../features/login/Login'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
 
 function App() {
 
     const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
-    
+
     return (
-        <div className="App">
-            <ErrorSnackbar />
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="secondary" aria-label="menu">
-                        <Menu />
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    <Button variant="contained" color="inherit">Login</Button>
-                </Toolbar>
-                {status === 'loading' && <LinearProgress color="secondary" />}
-            </AppBar>
-            <Container fixed>
-                <TodolistsList />
-            </Container>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <ErrorSnackbar />
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" color="secondary" aria-label="menu">
+                            <Menu />
+                        </IconButton>
+                        <Typography variant="h6">
+                            News
+                        </Typography>
+                        <Button variant="contained" color="inherit">Login</Button>
+                    </Toolbar>
+                    {status === 'loading' && <LinearProgress color="secondary" />}
+                </AppBar>
+                <Container fixed>
+                    <Routes>
+                        <Route path="/" element={<TodolistsList />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="404" element={<h1>404: PAGE NOT FOUND</h1>} />
+                        <Route path="*" element={<Navigate to="/404" />} />
+                    </Routes>
+                </Container>
+            </div>
+        </BrowserRouter>
     )
 }
 
--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import {Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
+import Grid from '@mui/material/Grid';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export const Login = () => {
-    return <Grid container justify="center">
-        <Grid item xs={4}>
+    return <Grid container justifyContent={'center'}>
+        <Grid item justifyContent={'center'}>
             <FormControl>
                 <FormLabel>
                     <p>To log in get registered
                         <a href={'https://social-network.samuraijs.com/'}
-                            target={'_blank'}>here
+                            target={'_blank'}> here
                         </a>
                     </p>
                     <p>or use common test account credentials:</p>
@@ -15,21 +23,14 @@ export const Login = () => {
                     <p>Password: free</p>
                 </FormLabel>
                 <FormGroup>
-                    <TextField
-                        label="Email"
+                    <TextField label="Email" margin="normal" />
+                    <TextField type="password" label="Password"
                         margin="normal"
                     />
-                    <TextField
-                        type="password"
-                        label="Password"
-                        margin="normal"
-                    />
-                    <FormControlLabel
-                        label={'Remember me'}
-                        control={<Checkbox />}
-                    />
-                    <Button type={'submit'} variant={'contained'}
-                        color={'primary'}>Login</Button>
+                    <FormControlLabel label={'Remember me'} control={<Checkbox />} />
+                    <Button type={'submit'} variant={'contained'} color={'primary'}>
+                        Login
+                    </Button>
                 </FormGroup>
             </FormControl>
         </Grid>
